d6b33e84eca6552c2abbd9c41bd8ebb99bdb8830
Edilson Francisco <fco_edilson@hotmail.com>
2020-01-29 16:10:46 -0300

    CRUD

--- start/routes.js
+++ start/routes.js
@@ -13,9 +13,10 @@
 |
 */
 
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.post('sessions', 'SessionController.store')
+
+Route.group(() => {
+  Route.resource('teams', 'TeamController').apiOnly()
+}).middleware('auth')
